9a630e7a64938eec9ad4b783378b17449ca2d237
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-14 23:52:32 -0700

    impl ApiUrls class

--- lib/controller/home_controller/home_controller.dart
+++ lib/controller/home_controller/home_controller.dart
@@ -1,5 +1,19 @@
+import 'package:flutter_techblog/core/services/web_service.dart';
+import 'package:flutter_techblog/model/home_model/home_model.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController{
+
+//================= variables ==================================================
+ TechBlogHomeDataModel? homeDataModel;
+ bool isLoading = false;
+
+//================= functions ==================================================
+
+  Future<dynamic> fetchHomeData() async {
+    isLoading = true;
+    update();
+    final response = await TechWebService.getRequest(url: '');
+  }
 
 }
---
+++ lib/core/constants/api_url/api_urls.dart
@@ -0,0 +1,4 @@
+class ApiUrls{
+  static String baseUrl = "https://techblog.sasansafari.com/Techblog/api/";
+  static String homeApi = "${baseUrl}home/?command=index";
+}
