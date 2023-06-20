27068bff9c7a298ea2a1c45ab16f9055e7c28f0b
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-19 22:34:22 -0700

    create SingleArticleController class

--- lib/core/controller_binding/app_controller_binding.dart
+++ lib/core/controller_binding/app_controller_binding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_techblog/controller/home_controller/home_controller.dart';
 import 'package:flutter_techblog/controller/navigation_controller/navigation_controller.dart';
+import 'package:flutter_techblog/controller/single_article_controller.dart';
 import 'package:get/get.dart';
 
 class ControllerBinding extends Bindings{
@@ -9,6 +10,8 @@ class ControllerBinding extends Bindings{
     Get.put(NavigationController());
     // home controller
     Get.lazyPut(() => HomeController());
+    // single article controller
+    Get.lazyPut(() => SingleArticleController());
   }
 
 }
