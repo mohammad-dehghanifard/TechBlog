bbc570581982a8ff1673129b38eb9420423a4099
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-22 01:48:04 -0700

    refactor ui and create verifyCodeBottomSheet

--- lib/view/register_screen/screens/register_intro_screen.dart
+++ lib/view/register_screen/screens/register_intro_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter_techblog/core/constants/texts/app_texts.dart';
 import 'package:flutter_techblog/gen/assets.gen.dart';
 import 'package:flutter_techblog/view/register_screen/widgets/bottom_sheet/register_bottom_sheet_widget.dart';
 import 'package:flutter_techblog/view/register_screen/widgets/btn/register_btn_widget.dart';
-import 'package:get/get.dart';
 
 class RegisterIntro extends StatelessWidget {
   const RegisterIntro({Key? key}) : super(key: key);
@@ -33,48 +32,7 @@ class RegisterIntro extends StatelessWidget {
                   // input email
                   registerBottomSheet(
                       context: context,
-                      size: size,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(AppString.inputEmail,style: ApplicationTextStyle.registerScreenTxtStyle,textAlign: TextAlign.center),
-                          SizedBox(height: size.height * 0.03),
-                          TextField(
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(
-                                hintStyle: ApplicationTextStyle.hintTxtTxtStyle,
-                                hintText: 'techblog@gmail.com'
-                            ),
-                          ),
-                          SizedBox(height: size.height * 0.06),
-                          // active user
-                          RegisterBtn(size: size, text: "ادامه", onTap: () {
-                           Get.back();
-                           registerBottomSheet(
-                               context: context,
-                               size: size,
-                               child: Column(
-                                 mainAxisAlignment: MainAxisAlignment.center,
-                                 children: [
-                                   Text(AppString.inputActiveCode,style: ApplicationTextStyle.registerScreenTxtStyle,textAlign: TextAlign.center),
-                                   SizedBox(height: size.height * 0.03),
-                                   TextField(
-                                     textAlign: TextAlign.center,
-                                     keyboardType: TextInputType.number,
-                                     decoration: InputDecoration(
-                                         hintStyle: ApplicationTextStyle.hintTxtTxtStyle,
-                                         hintText: '******'
-                                     ),
-                                   ),
-                                   SizedBox(height: size.height * 0.06),
-                                   RegisterBtn(size: size, text: "ادامه", onTap: () => Get.back()
-                                   )
-                                 ],
-                               ) );
-                          }
-                          )
-                        ],
-                      ) );
+                      size: size,);
                 }
             )
           ],
--- lib/view/register_screen/widgets/bottom_sheet/verify_code_bottom_sheet.dart
+++ lib/view/register_screen/widgets/bottom_sheet/verify_code_bottom_sheet.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_techblog/core/constants/style/text_styles.dart';
 import 'package:flutter_techblog/core/constants/texts/app_texts.dart';
+import 'package:flutter_techblog/view/register_screen/widgets/btn/register_btn_widget.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
-import '../btn/register_btn_widget.dart';
-import 'verify_code_bottom_sheet.dart';
-
-Future<dynamic> registerBottomSheet({ required BuildContext context, required Size size}) {
+Future<dynamic> verifyCodeBottomSheet({ required BuildContext context, required Size size}) {
   return showModalBottomSheet(
     context: context,
     backgroundColor: Colors.transparent,
@@ -24,25 +21,21 @@ Future<dynamic> registerBottomSheet({ required BuildContext context, required Si
           child:  Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(AppString.inputEmail,style: ApplicationTextStyle.registerScreenTxtStyle,textAlign: TextAlign.center),
+              Text(AppString.inputActiveCode,style: ApplicationTextStyle.registerScreenTxtStyle,textAlign: TextAlign.center),
               SizedBox(height: size.height * 0.03),
               TextField(
                 textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                     hintStyle: ApplicationTextStyle.hintTxtTxtStyle,
-                    hintText: 'techblog@gmail.com'
+                    hintText: '******'
                 ),
               ),
               SizedBox(height: size.height * 0.06),
-              // active user
-              RegisterBtn(size: size, text: "ادامه", onTap: () {
-                Get.back();
-                verifyCodeBottomSheet(context: context,size: size);
-              }
+              RegisterBtn(size: size, text: "ادامه", onTap: () => Get.back()
               )
             ],
           )
       );
     },);
 }
-
