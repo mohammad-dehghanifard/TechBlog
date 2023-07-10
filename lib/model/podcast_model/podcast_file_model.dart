b10b68d7308c99ce669663cafa7fe659b734f164
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-10 02:11:23 -0700

    impl PodcastFile Model

---
+++ lib/model/podcast_model/podcast_file_model.dart
@@ -0,0 +1,15 @@
+class PodcastFileModel{
+  String? id;
+  String? podcastId;
+  String? file;
+  String? title;
+  String? length;
+
+  PodcastFileModel.fromJson(Map<String,dynamic> json){
+    id = json["id"];
+    podcastId = json["podcast_id"];
+    file = json["file"];
+    title = json["title"];
+    length = json["length"];
+  }
+}
